0bc0b3031844accf300a9af1e23f2b465383d5db
rollerory <101998210+rollerory@users.noreply.github.com>
2025-03-14 09:27:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Policy from "@/pages/Policy";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Modal from "./components/modal/Modal";
-import ScrollToTop from "./utils/ScrollToTop";
 import ScrollToHashElement from "./utils/ScrollToHashElement";
 import { ModalProvider } from "./context/ModalContext";
 import "@/styles/style.scss";
@@ -14,7 +13,6 @@ function App() {
     return (
         <ModalProvider>
             <HashRouter basename="/">
-                <ScrollToTop />
                 <ScrollToHashElement />
                 <div className="wrapper">
                     <Header />
