ad6ec3c7508279b6e393c7a2f3b94253c958ccad
rollerory <101998210+rollerory@users.noreply.github.com>
2025-03-14 09:27:15 +0200

    Update ScrollToHashElement.jsx

--- src/utils/ScrollToHashElement.jsx
+++ src/utils/ScrollToHashElement.jsx
@@ -1,19 +1,21 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
-const ScrollToHashElement = () => {
-    const location = useLocation();
-
-    useEffect(() => {
-        if (location.hash) {
-            const element = document.getElementById(location.hash.substring(1));
-            if (element) {
-                element.scrollIntoView({ behavior: "smooth", block: "center" });
-            }
-        }
-    }, [location]);
-
-    return null;
-};
-
+import { useEffect } from "react"; 
+import { useLocation } from "react-router-dom"; 
+ 
+const ScrollToHashElement = () => { 
+    const location = useLocation(); 
+ 
+    useEffect(() => { 
+        if (location.hash) { 
+            const element = document.getElementById(location.hash.substring(1)); 
+            if (element) { 
+                element.scrollIntoView({ behavior: "smooth", block: "center" }); 
+            } 
+        } else { 
+            window.scrollTo({ top: 0, behavior: "smooth" }); 
+        } 
+    }, [location]); 
+ 
+    return null; 
+}; 
+ 
 export default ScrollToHashElement;
