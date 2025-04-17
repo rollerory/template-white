faa509494a1e4998f4b39a34c5f9ba4708ae5fbc
rollerory <101998210+rollerory@users.noreply.github.com>
2025-04-17 15:26:38 +0300

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Footer.scss"
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__inner">
+                    <h2>footer here</h2>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import LogoImg from "@/assets/images/logo.svg";
+import "./Logo.scss"
+
+export default function Logo() {
+    return (
+        <Link to="/" className="logo">
+            <img src={LogoImg} alt="Logo icon" />
+            <span>Company Name</span>
+        </Link>
+    )
+}
